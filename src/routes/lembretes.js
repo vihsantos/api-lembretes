1272dffcb0961a92da9e4d26b77e3f8e3586973c
Vitoria <vick_santos_vick@hotmail.com>
2021-10-01 23:08:28 -0300

    Nova router

--- src/routes/lembretes.js
+++ src/routes/lembretes.js
@@ -2,3 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db').pool;
 
+router.get('/', ""); // Pegar todos os lembretes
+router.get('/:id', ""); // Pegar o lembrete por id
+router.post('/', ""); // Criar um novo lembrete
+router.put('/:id', "") // Editar um lembrete
