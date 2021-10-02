96fba4928a43fa2a49ff45fb3564acd5ce2c400d
Vitoria <vick_santos_vick@hotmail.com>
2021-10-01 23:42:40 -0300

    remover constante não usada

--- src/routes/lembretes.js
+++ src/routes/lembretes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db').pool;
 
 router.get('/', ""); // Pegar todos os lembretes
 router.get('/:id', ""); // Pegar o lembrete por id
