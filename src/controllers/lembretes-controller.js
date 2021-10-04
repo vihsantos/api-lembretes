f56cea51daa2ff1661f6af5ea9a81778bad39f05
Vitoria <vick_santos_vick@hotmail.com>
2021-10-04 19:15:55 -0300

    comentando o criar lembrete

--- src/controllers/lembretes-controller.js
+++ src/controllers/lembretes-controller.js
@@ -18,11 +18,5 @@ exports.pegarLembrete = (req, res) => {
       })
 };
 exports.criarLembrete = (req, res) => {
-    const {id, titulo, descricao, datal} = req.body;
-    db.query('INSERT into lembretes (id, titulo, descricao, datal) values ($1, $2, $3, $4)', [id, titulo, descricao, datal], (error, results) => {
-        if (error) {
-          throw error
-        }
-        res.status(201).send('Lembrete adicionado com sucesso')
-      })
+//
 };
